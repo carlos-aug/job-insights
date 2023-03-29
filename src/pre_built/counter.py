d43349172bf3db743acb0940012de79fa707f295
Carlos Augusto <caarlos.san@gmail.com>
2023-03-28 21:57:57 -0300

    add count word python

--- src/pre_built/counter.py
+++ src/pre_built/counter.py
@@ -2,4 +2,11 @@ def count_ocurrences(path: str, word: str) -> int:
     file = open(path, "r")
     read_data = file.read()
     word_count = read_data.lower().count(word.lower())
+
+    print(word_count)
+
     return word_count
+
+
+if __name__ == "__main__":
+    print(count_ocurrences('data/jobs.csv', 'Python'))
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,4 +4,4 @@ from src.pre_built.counter import count_ocurrences
 def test_counter():
     path = 'data/jobs.csv'
     word = 'Python'
-    assert count_ocurrences(path, word)
+    assert count_ocurrences(path, word) == 1639
